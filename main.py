ae48bbea6fa2d7533c4ab5ec5ad860ddfb5d9215
jbounds101 <jackson.bounds@gmail.com>
2022-03-30 22:52:41 -0400

    solve() function works!

--- main.py
+++ main.py
@@ -13,28 +13,54 @@ class SudokuBoard:
         '''
         self.sub_sections = []
 
+    def __str__(self):
+        self_string = ''
+        for i in range(9):
+            for j in range(9):
+                # i = row, j = column
+                pass  # todo finish
+
     def solve(self):
         solve_board = SudokuBoard(copy.deepcopy(self.arr))
-        i = 0
-        j = 0
-        while i <= 9:
-            while j <= 9:
-                if solve_board.arr[i][j] == 0:
-                    if solve_board.attempt_insert_num(j, i) is None:
+        attempted_nums = []
+        for i in range(82):
+            attempted_nums.append([])
 
+        solution_stack = []
+        i = 0
+        while i < 9:
+            j = 0
+            while j < 9:
+                if solve_board.arr[i][j] == 0:
+                    insert_num = solve_board.attempt_insert_num(j, i, attempted_nums[(i * 9) + j])
+                    if insert_num == -1:
+                        # No solution for the current location, pop stack
+                        popped = solution_stack.pop()
+                        j = popped[0]
+                        i = popped[1]
+                        solve_board.arr[i][j] = 0
+                        SudokuBoard.cleanse_attempted_after(i, j, attempted_nums)
+
+                        continue
+                    else:
+                        solution_stack.append([j, i, insert_num])
+                        attempted_nums[(i * 9) + j].append(insert_num)
                 j += 1
             i += 1
+        return solve_board
 
-    def attempt_insert_num(self, x, y):
+    def attempt_insert_num(self, x, y, ignore_vals):
         """
             Tries to insert 1 in the location specified, if it cannot, tries 2 ...
             If it cannot insert any number, returns None
         """
         for i in range(1, 10):
+            if i in ignore_vals:
+                continue
             if self.valid_insert(x, y, i):
                 self.arr[y][x] = i
-                return [y, x]
-        return None
+                return i
+        return -1
 
     def valid_insert(self, x, y, n):
         """ Check if inserting n at (x, y) results in a valid Sudoku board """
@@ -91,6 +117,13 @@ class SudokuBoard:
         return sections
 
     @staticmethod
+    def cleanse_attempted_after(i, j, to_clean):
+        """ Removes all values of attempted_nums after (j, i) """
+        start_index = (i * 9) + j + 1
+        for i in range(start_index, 82):
+            to_clean[i].clear()
+
+    @staticmethod
     def get_sub_section(x, y):
         """ Returns the sub_section of the given (x,y) coordinate """
         coord = {
@@ -102,22 +135,22 @@ class SudokuBoard:
         column = coord[x]
 
         return {
-            (0, 0): 0, (0, 1): 2, (0, 2): 3,
-            (1, 0): 4, (1, 1): 5, (1, 2): 6,
-            (2, 0): 7, (2, 1): 8, (2, 2): 9,
+            (0, 0): 0, (0, 1): 1, (0, 2): 2,
+            (1, 0): 3, (1, 1): 4, (1, 2): 5,
+            (2, 0): 6, (2, 1): 7, (2, 2): 8
         }[row, column]
 
 
 sudokuArr = [
-       [5, 3, 0, 0, 7, 0, 0, 0, 0],
-       [6, 0, 0, 1, 9, 5, 0, 0, 0],
-       [0, 9, 8, 0, 0, 0, 0, 6, 0],
-       [8, 0, 0, 0, 6, 0, 0, 0, 3],
-       [4, 0, 0, 8, 0, 3, 0, 0, 1],
-       [7, 0, 0, 0, 2, 0, 0, 0, 6],
-       [0, 6, 0, 0, 0, 0, 2, 8, 0],
-       [0, 0, 0, 4, 1, 9, 0, 0, 5],
-       [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 sudoku_board = SudokuBoard(sudokuArr)
 sudoku_board.solve()
