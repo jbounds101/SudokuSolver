9e1028bb63a4e3be32e1805ee4de280377efff0c
jbounds101 <jackson.bounds@gmail.com>
2022-04-01 21:02:40 -0400

    Added another test array

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ class SudokuBoard:
             j = 0
             while j < 9:
                 if solve_board.arr[i][j] == 0:
-                    insert_num = solve_board.attempt_insert_num(j, i, attempted_nums[(i * 9) + j])
+                    insert_num = solve_board.__attempt_insert_num(j, i, attempted_nums[(i * 9) + j])
                     if insert_num == -1:
                         # No solution for the current location, pop stack
                         popped = solution_stack.pop()
                         j = popped[0]
                         i = popped[1]
                         solve_board.arr[i][j] = 0
-                        SudokuBoard.cleanse_after(i, j, attempted_nums)
+                        SudokuBoard.__cleanse_after(i, j, attempted_nums)
 
                         continue
                     else:
@@ -60,51 +60,6 @@ class SudokuBoard:
                 j += 1
             i += 1
         return solve_board.arr
-
-    def attempt_insert_num(self, x, y, ignore_vals):
-        """
-            Tries to insert 1 in the location specified, if it cannot, tries 2 ...
-            If it cannot insert any number, returns None
-        """
-        for i in range(1, 10):
-            if i in ignore_vals:
-                continue
-            if self.valid_insert(x, y, i):
-                self.arr[y][x] = i
-                return i
-        return -1
-
-    def valid_insert(self, x, y, n):
-        """ Check if inserting n at (x, y) results in a valid Sudoku board """
-
-        # Go across row and column simultaneously
-        for i in range(9):
-            if (self.arr[y][i] == n) and (i != x):
-                return False
-            if (self.arr[i][x] == n) and (i != y):
-                return False
-
-        # Check sub_section
-        section_num = SudokuBoard.get_sub_section(x, y)
-        row_index = {
-            0: 0, 1: 0, 2: 0,
-            3: 3, 4: 3, 5: 3,
-            6: 6, 7: 6, 8: 6
-        }[section_num]
-        column_index = {
-            0: 0, 1: 3, 2: 6,
-            3: 0, 4: 3, 5: 6,
-            6: 0, 7: 3, 8: 6
-        }[section_num]
-        for i in range(row_index, row_index + 3):
-            for j in range(column_index, column_index + 3):
-                if (i == y) and (j == x):
-                    # Skip the value we are looking to change
-                    continue
-                if self.arr[i][j] == n:
-                    return False
-
-        return True
 
     def get_sub_sections(self):
         """ Returns the sub_sections of the sudoku board """
@@ -127,15 +82,60 @@ class SudokuBoard:
             sections.append(current_section)
         return sections
 
+    def __attempt_insert_num(self, x, y, ignore_vals):
+        """
+            Tries to insert 1 in the location specified, if it cannot, tries 2 ...
+            If it cannot insert any number, returns None
+        """
+        for i in range(1, 10):
+            if i in ignore_vals:
+                continue
+            if self.__valid_insert(x, y, i):
+                self.arr[y][x] = i
+                return i
+        return -1
+
+    def __valid_insert(self, x, y, n):
+        """ Check if inserting n at (x, y) results in a valid Sudoku board """
+
+        # Go across row and column simultaneously
+        for i in range(9):
+            if (self.arr[y][i] == n) and (i != x):
+                return False
+            if (self.arr[i][x] == n) and (i != y):
+                return False
+
+        # Check sub_section
+        section_num = SudokuBoard.__get_sub_section(x, y)
+        row_index = {
+            0: 0, 1: 0, 2: 0,
+            3: 3, 4: 3, 5: 3,
+            6: 6, 7: 6, 8: 6
+        }[section_num]
+        column_index = {
+            0: 0, 1: 3, 2: 6,
+            3: 0, 4: 3, 5: 6,
+            6: 0, 7: 3, 8: 6
+        }[section_num]
+        for i in range(row_index, row_index + 3):
+            for j in range(column_index, column_index + 3):
+                if (i == y) and (j == x):
+                    # Skip the value we are looking to change
+                    continue
+                if self.arr[i][j] == n:
+                    return False
+
+        return True
+
     @staticmethod
-    def cleanse_after(i, j, to_clean):
+    def __cleanse_after(i, j, to_clean):
         """ Removes all values from to_clean (list of lists) after (j, i) """
         start_index = (i * 9) + j + 1
         for i in range(start_index, 82):
             to_clean[i].clear()
 
     @staticmethod
-    def get_sub_section(x, y):
+    def __get_sub_section(x, y):
         """ Returns the sub_section of the given (x,y) coordinate """
         coord = {
             0: 0, 1: 0, 2: 0,
@@ -163,10 +163,25 @@ sudokuArr = [
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
-sudoku_board = SudokuBoard(sudokuArr)
+
+other_sudoku_arr = [
+    [0, 7, 5, 0, 9, 0, 0, 0, 6],
+    [0, 2, 3, 0, 8, 0, 0, 4, 0],
+    [8, 0, 0, 0, 0, 3, 0, 0, 1],
+    [5, 0, 0, 7, 0, 2, 0, 0, 0],
+    [0, 4, 0, 8, 0, 6, 0, 2, 0],
+    [0, 0, 0, 9, 0, 1, 0, 0, 3],
+    [9, 0, 0, 4, 0, 0, 0, 0, 7],
+    [0, 6, 0, 0, 7, 0, 5, 8, 0],
+    [7, 0, 0, 0, 1, 0, 3, 9, 0]
+]
+sudoku_board = SudokuBoard(other_sudoku_arr)
+
 print(sudoku_board)
+
 start = time.time()
 sudoku_board.arr = sudoku_board.solve()
 print(sudoku_board)
 end = time.time()
 print(end - start)
+
